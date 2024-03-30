403231bd8d162a4e1ce5654996a4e34dc31879d4
AlexEngo <a.o.engovatov@yandex.ru>
2024-03-30 19:28:47 +0400

    add Category db model

---
+++ backend/models/Category.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const CategorySchema = mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        imageUrl: {
+            type: String,
+            required: true,
+        },
+        color: {
+            type: String,
+            required: true,
+        },
+    },
+    { timestamps: true }
+);
+
+const Category = mongoose.model("Category", CategorySchema);
+
+module.exports = Category;
