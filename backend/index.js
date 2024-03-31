708a2b105a3a59c8906f73f6bbf4c55c5a055313
AlexEngo <a.o.engovatov@yandex.ru>
2024-03-31 17:46:04 +0400

    fix bug

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const userRoute = require("./routes/user.routes");
 const userForAdminRoute = require("./routes/user-for-admin.routes");
