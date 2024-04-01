7f7c3abd835fa302bf9b5e20e1f76839e01c0bcd
AlexEngo <a.o.engovatov@yandex.ru>
2024-04-01 20:33:26 +0400

    add pagination on product.service

--- backend/services/product.service.js
+++ backend/services/product.service.js
@@ -1,5 +1,27 @@
 const Product = require("../models/Product");
 
+exports.getProduct = (id) => {
+    return Product.find(id).populate("category");
+};
+
+exports.getProducts = async (
+    search = "",
+    limit = 10,
+    page = 1,
+    categoryId,
+    priceSort = false
+) => {
+    const [products, count] = await Promise.all([
+        Product.find({ name: { $regex: search, $options: "i" }, categoryId })
+            .limit(limit)
+            .skip((page - 1) * limit)
+            .sort({ price: priceSort ? 1 : -1 }),
+        Product.countDocuments({ name: { $regex: search, $options: "i" }, categoryId }),
+    ]);
+
+    return { products, lastPage: Math.ceil(count / limit) };
+};
+
 exports.addProduct = async (
     name,
     categoryId,
