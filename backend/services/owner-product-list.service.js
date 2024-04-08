200912ed36f7ce5d590d04daa441290c3b63dfe7
AlexEngo <a.o.engovatov@yandex.ru>
2024-04-08 16:12:08 +0400

    updated values ownerProducts in double added

--- backend/services/owner-product-list.service.js
+++ backend/services/owner-product-list.service.js
@@ -8,22 +8,37 @@ exports.addOwnerProduct = async (owner, product, status, count) => {
     const isOwnerProductListExist = await Boolean(
         OwnerProductList.countDocuments({ owner })
     );
-    let ownerProduct = {};
+
+    let newOwnerProduct = {};
 
     if (isOwnerProductListExist) {
-        ownerProduct = await OwnerProductList.findOneAndUpdate(
-            { owner },
-            { $push: { products: { product, count, status } } },
-            { returnDocument: "after" }
-        );
+        const oldProducts = await OwnerProductList.findOne({
+            owner,
+            "products.product": product,
+        });
+
+        if (oldProducts) {
+            newOwnerProduct = await OwnerProductList.findOneAndUpdate(
+                { owner, "products.product": product },
+                { $set: { "products.$.count": count, "products.$.status": status } },
+                { returnDocument: "after" }
+            );
+        } else {
+            newOwnerProduct = await OwnerProductList.findOneAndUpdate(
+                { owner },
+                { $push: { products: { product, count, status } } },
+                { returnDocument: "after" }
+            );
+        }
+        
     } else {
-        ownerProduct = await OwnerProductList.create({
+        newOwnerProduct = await OwnerProductList.create({
             owner,
             products: [{ product, status, count }],
         });
     }
 
-    return ownerProduct;
+    return newOwnerProduct;
 };
 
 exports.deleteOwnerProduct = async (owner, product) => {
