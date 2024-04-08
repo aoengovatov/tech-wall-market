adb6a874626eb20c9f0d0e61e0000806db81e43f
AlexEngo <a.o.engovatov@yandex.ru>
2024-04-08 15:09:16 +0400

    added or created in ownerProductList

--- backend/services/owner-product-list.service.js
+++ backend/services/owner-product-list.service.js
@@ -5,10 +5,23 @@ exports.getOwnerProducts = (userId) => {
 };
 
 exports.addOwnerProduct = async (owner, product, status, count) => {
-    const ownerProduct = await OwnerProductList.create({
-        owner,
-        products: [{ product, status, count }],
-    });
+    const isOwnerProductListExist = await Boolean(
+        OwnerProductList.countDocuments({ owner })
+    );
+    let ownerProduct = {};
+
+    if (isOwnerProductListExist) {
+        ownerProduct = await OwnerProductList.findOneAndUpdate(
+            { owner },
+            { $push: { products: { product, count, status } } },
+            { returnDocument: "after" }
+        );
+    } else {
+        ownerProduct = await OwnerProductList.create({
+            owner,
+            products: [{ product, status, count }],
+        });
+    }
 
     return ownerProduct;
 };
