2a99db65646f901a42bf4031eb63466c9414a7ab
AlexEngo <a.o.engovatov@yandex.ru>
2024-04-23 19:12:10 +0400

    add user page styled

---
+++ frontend/src/pages/users/components/user-roll/user-roll.jsx
@@ -0,0 +1,17 @@
+export const UserRoll = ({ login, registeredAt, role }) => {
+    const isSave = true;
+    return (
+        <div className="flex items-center">
+            <div className="grid grid-cols-3 gap-y-2 bg-lightGray p-[5px] mb-[3px] rounded-lg w-[80%]">
+                <div className="">ivan1994</div>
+                <div className="">2020-07-03 15:23</div>
+                <select>
+                    <option>Покупатель</option>
+                    <option>Модератор</option>
+                    <option>Администратор</option>
+                </select>
+            </div>
+            {isSave && <div className="ml-[5px]">Сохранить</div>}
+        </div>
+    );
+};
--- frontend/src/pages/users/users.jsx
+++ frontend/src/pages/users/users.jsx
@@ -1,3 +1,48 @@
+import { Breadcrumbs } from "../../components";
+import { UserRoll } from "./components/user-roll/user-roll";
+
+const users = [
+    {
+        id: "33513",
+        login: "ivan1994",
+        registeredAt: "2019-05-03 21:42",
+        role: "Покупатель",
+    },
+    {
+        id: "123",
+        login: "svetlanaSR",
+        registeredAt: "2020-07-03 15:23",
+        role: "Модератор",
+    },
+    {
+        id: "1342",
+        login: "igorBoldin",
+        registeredAt: "2019-05-03 23:12",
+        role: "Администратор",
+    },
+];
+
 export const Users = () => {
-    return <div>Пользователи (изменение ролей)</div>;
+    return (
+        <>
+            <Breadcrumbs />
+            <div className="mb-[30px]">
+                <h1 className="ml-[10px] mb-[10px]">Пользователи</h1>
+                <div className="grid grid-cols-3 mb-[5px] px-[5px] w-[80%]">
+                    <div className="text-gray">Логин</div>
+                    <div className="text-gray">Дата создания</div>
+                    <div className="text-gray">Роль</div>
+                    <div></div>
+                </div>
+                {users.map(({ id, login, registeredAt, role }) => (
+                    <UserRoll
+                        key={id}
+                        login={login}
+                        registeredAt={registeredAt}
+                        role={role}
+                    />
+                ))}
+            </div>
+        </>
+    );
 };
