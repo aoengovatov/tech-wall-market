d1dd227ca473192a18d467e7031bc6ee9dda8111
AlexEngo <a.o.engovatov@yandex.ru>
2024-04-23 19:29:36 +0400

    fix user page style

--- frontend/src/pages/users/components/user-roll/user-roll.jsx
+++ frontend/src/pages/users/components/user-roll/user-roll.jsx
@@ -1,5 +1,6 @@
 export const UserRoll = ({ login, registeredAt, role }) => {
     const isSave = true;
+
     return (
         <div className="flex items-center">
             <div className="grid grid-cols-3 gap-y-2 bg-lightGray p-[5px] mb-[3px] rounded-lg w-[80%]">
@@ -11,7 +12,11 @@ export const UserRoll = ({ login, registeredAt, role }) => {
                     <option>Администратор</option>
                 </select>
             </div>
-            {isSave && <div className="ml-[5px]">Сохранить</div>}
+            {isSave && (
+                <div className="ml-[10px] text-blue cursor-pointer hover:underline">
+                    Сохранить
+                </div>
+            )}
         </div>
     );
 };
