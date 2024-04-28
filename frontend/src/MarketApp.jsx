cc69cab0b6ab6ec441ed2c1eac2a90b1b537bba7
AlexEngo <a.o.engovatov@yandex.ru>
2024-04-28 16:33:50 +0400

    fix footer bottom

--- frontend/src/MarketApp.jsx
+++ frontend/src/MarketApp.jsx
@@ -22,29 +22,43 @@ export const MarketApp = () => {
     return (
         <>
             <Modal on={false} />
-            <Header />
-            <div className="w-[1100px] mx-auto">
-                <div className="mx-[20px]">
-                    <Routes>
-                        <Route path="/" element={<Main />} />
-                        <Route path="/login" element={<Authorization />} />
-                        <Route path="/register" element={<Registration />} />
-                        <Route path="/catalog" element={<Catalog />} />
-                        <Route path="/catalog/:id" element={<SingleProduct />} />
-                        <Route path="/profile" element={<Profile />} />
-                        <Route path="/profile/favorites" element={<Favorites />} />
-                        <Route path="/profile/basket" element={<Basket />} />
-                        <Route path="/profile/orders" element={<Orders />} />
-                        <Route path="/profile/my-orders" element={<MyOrders />} />
-                        <Route path="/profile/add-product" element={<AddProduct />} />
-                        <Route path="/profile/category" element={<AddCategory />} />
-                        <Route path="/profile/users" element={<Users />} />
-                        <Route path="*" element={<Page404 />} />
-                    </Routes>
-                </div>
-            </div>
+            <div className="flex flex-col min-h-[100vh] justify-between">
+                <div>
+                    <Header />
 
-            <Footer />
+                    <div className="w-[1100px] mx-auto">
+                        <div className="mx-[20px]">
+                            <Routes>
+                                <Route path="/" element={<Main />} />
+                                <Route path="/login" element={<Authorization />} />
+                                <Route path="/register" element={<Registration />} />
+                                <Route path="/catalog" element={<Catalog />} />
+                                <Route path="/catalog/:id" element={<SingleProduct />} />
+                                <Route path="/profile" element={<Profile />} />
+                                <Route
+                                    path="/profile/favorites"
+                                    element={<Favorites />}
+                                />
+                                <Route path="/profile/basket" element={<Basket />} />
+                                <Route path="/profile/orders" element={<Orders />} />
+                                <Route path="/profile/my-orders" element={<MyOrders />} />
+                                <Route
+                                    path="/profile/add-product"
+                                    element={<AddProduct />}
+                                />
+                                <Route
+                                    path="/profile/category"
+                                    element={<AddCategory />}
+                                />
+                                <Route path="/profile/users" element={<Users />} />
+                                <Route path="*" element={<Page404 />} />
+                            </Routes>
+                        </div>
+                    </div>
+                </div>
+
+                <Footer />
+            </div>
         </>
     );
 };
