a90bc7e5cefc958f008e6fd9405bed2f390f7ccf
AlexEngo <a.o.engovatov@yandex.ru>
2024-04-29 18:03:31 +0400

    change state button save in user-role component

--- frontend/src/pages/users/components/user-roll/user-roll.jsx
+++ frontend/src/pages/users/components/user-roll/user-roll.jsx
@@ -1,21 +1,29 @@
 import { useState } from "react";
 
 export const UserRoll = ({ login, registedAt, roleId, roles }) => {
-    const isSave = true;
     const [initialRoleId, setInitialRoleId] = useState(roleId);
+    const [updatedRoleId, setUpdatedRoleId] = useState(roleId);
+
+    const onRoleChange = ({ target }) => {
+        setUpdatedRoleId(Number(target.value));
+    };
+
+    const isSaveRole = initialRoleId !== updatedRoleId;
 
     return (
         <div className="flex items-center">
             <div className="grid grid-cols-3 gap-y-2 bg-lightGray p-[5px] mb-[3px] rounded-lg w-[80%]">
                 <div className="">{login}</div>
                 <div className="">{registedAt}</div>
-                <select value={initialRoleId}>
+                <select value={updatedRoleId} onChange={(target) => onRoleChange(target)}>
                     {roles.map(({ id, name }) => (
-                        <option key={id} value={id}>{name}</option>
+                        <option key={id} value={id}>
+                            {name}
+                        </option>
                     ))}
                 </select>
             </div>
-            {isSave && (
+            {isSaveRole && (
                 <div className="ml-[10px] text-blue cursor-pointer hover:underline">
                     Сохранить
                 </div>
