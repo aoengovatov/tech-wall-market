3b39a528dc9d99f7365d15b972ca453015fbfd2b
AlexEngo <a.o.engovatov@yandex.ru>
2024-05-01 22:46:35 +0400

    refactoring breadcrumbs

--- frontend/src/components/breadcrumbs/breadcrumbs.jsx
+++ frontend/src/components/breadcrumbs/breadcrumbs.jsx
@@ -1,8 +1,32 @@
+import { Link } from "react-router-dom";
+
 export const Breadcrumbs = () => {
+    const path = window.location.pathname.slice(1).split("/");
+    const pathMap = [];
+
+    path.forEach((path, index) => {
+        if (index === 0) {
+            pathMap.push("/" + path);
+        } else {
+            if (path[index-1] === "catalog") {
+                pathMap.push(pathMap[index - 1] + "/" + path);
+            }
+        }
+    });
+
     return (
         <div className="flex ml-[10px] font-xs text-darkGray">
-            Главная <div className="mx-[5px]"> / </div>
-            <span className="text-blue">Каталог</span>
+            <Link to={"/"}>
+                <span>home</span>
+            </Link>
+            {pathMap.map((pathLink, index) => (
+                <>
+                    <div className="mx-[5px]"> / </div>
+                    <Link to={pathLink}>
+                        <span>{path[index]}</span>
+                    </Link>
+                </>
+            ))}
         </div>
     );
 };
