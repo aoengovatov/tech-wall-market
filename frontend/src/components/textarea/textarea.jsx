93b86e419decdfa36888dc7d3895e01cd073442e
AlexEngo <a.o.engovatov@yandex.ru>
2024-05-03 19:25:23 +0400

    add-product page refactoring

--- frontend/src/components/textarea/textarea.jsx
+++ frontend/src/components/textarea/textarea.jsx
@@ -1,9 +1,13 @@
-export const Textarea = ({ placeholder, required }) => {
+import { forwardRef } from "react";
+
+export const Textarea = forwardRef(({ placeholder, required, ...props }, ref) => {
     return (
         <textarea
-            className="outline-none text-darkGray px-[10px] py-[5px] mb-[5px] border-2 border-transparent bg-lightGray rounded-lg focus:border-blue"
+            {...props}
+            className="outline-none min-h-[100px] text-darkGray px-[10px] py-[5px] mb-[5px] border-2 border-transparent bg-lightGray rounded-lg focus:border-blue"
             placeholder={placeholder}
             required={required === true ? "required" : ""}
+            ref={ref}
         ></textarea>
     );
-};
+});
--- frontend/src/pages/add-product/add-product.jsx
+++ frontend/src/pages/add-product/add-product.jsx
@@ -1,40 +1,133 @@
 import { Breadcrumbs, ButtonBlue, Input, Textarea, BackBtn } from "../../components";
+import { request } from "../../utils";
 import { useNavigate } from "react-router-dom";
+import { useEffect, useState, useRef } from "react";
 
 export const AddProduct = () => {
     const navigate = useNavigate();
+    const [serverError, setServerError] = useState("");
+    const [categories, setCategories] = useState([]);
+    const name = useRef("");
+    const categoryId = useRef("");
+    const imageUrl = useRef("");
+    const oldPrice = useRef(0);
+    const price = useRef(0);
+    const count = useRef(0);
+    const description = useRef("");
+    const characteristic = useRef("");
+
+    useEffect(() => {
+        request("/categories").then((data) => {
+            if (data.error === null) {
+                setCategories(data.categories);
+            }
+        });
+    }, []);
+
+    const addNewProduct = (event) => {
+        event.preventDefault();
+
+        const product = {
+            name: name.current.value,
+            category: categoryId.current.value,
+            imageUrl: imageUrl.current.value,
+            oldPrice: oldPrice.current.value,
+            price: price.current.value,
+            count: count.current.value,
+            description: description.current.value,
+            characteristic: characteristic.current.value,
+        };
+
+        request("/products", "POST", product).then((data) => {
+            if (data.error) {
+                setServerError(`Ошибка: ${data.error}`);
+                return;
+            }
+            console.log("Создан продукт", data.product);
+            name.current.value = "";
+            categoryId.current.value = "";
+            imageUrl.current.value = "";
+            oldPrice.current.value = 0;
+            price.current.value = 0;
+            count.current.value = 0;
+            description.current.value = "";
+            characteristic.current.value = "";
+        });
+    };
 
     return (
         <>
             <Breadcrumbs />
-            <div className="mb-[20px] max-w-[50%]">
+            <div className="mb-[20px] w-full">
                 <div className="flex items-center mb-[10px]">
                     <BackBtn onClick={() => navigate(-1)} />
                     <h1 className="ml-[10px]">Добавить товар</h1>
                 </div>
-                <form className="flex flex-col mb-[30px]">
+                {serverError && <div>{serverError}</div>}
+                <form
+                    onSubmit={addNewProduct}
+                    className="flex md:w-[60%] w-full mx-auto flex-col mb-[30px] gap-y-1 "
+                >
                     <Input
                         type={"text"}
+                        onChange={() => setServerError("")}
+                        ref={name}
                         placeholder={"название товара..."}
                         required={true}
                     />
-                    <select className="outline-none text-darkGray px-[10px] py-[5px] mb-[5px] border-2 border-transparent bg-lightGray rounded-lg focus:border-blue">
-                        <option selected disabled hidden>
-                            категория товара
-                        </option>
-                        <option>ноутбуки</option>
-                        <option>смартфоны</option>
-                        <option>телевизоры</option>
-                        <option>смарт-часы</option>
-                        <option>умные колонки</option>
+                    <select
+                        ref={categoryId}
+                        onChange={() => setServerError("")}
+                        className="outline-none text-darkGray px-[10px] py-[5px] border-2 border-transparent bg-lightGray rounded-lg focus:border-blue"
+                    >
+                        {categories.map(({ id, name }) => (
+                            <option key={id} value={id}>
+                                {name}
+                            </option>
+                        ))}
                     </select>
-                    <Input type={"text"} placeholder={"url картинки..."} />
-                    <Input type={"text"} placeholder={"цена со скидкой"} />
-                    <Input type={"text"} placeholder={"цена без скидки"} />
-                    <Input type={"text"} placeholder={"количество товара"} />
-                    <Textarea placeholder={"Описание товара"} />
-                    <Textarea placeholder={"характеристики товара"} />
-                    <ButtonBlue type={"submit"}>добавить</ButtonBlue>
+                    <Input
+                        type={"text"}
+                        onChange={() => setServerError("")}
+                        ref={imageUrl}
+                        placeholder={"url картинки..."}
+                        required={true}
+                    />
+                    <Input
+                        type={"number"}
+                        onChange={() => setServerError("")}
+                        ref={oldPrice}
+                        placeholder={"цена со скидкой"}
+                    />
+                    <Input
+                        type={"number"}
+                        onChange={() => setServerError("")}
+                        ref={price}
+                        placeholder={"цена без скидки"}
+                        required={true}
+                    />
+                    <Input
+                        type={"number"}
+                        onChange={() => setServerError("")}
+                        ref={count}
+                        placeholder={"количество товара"}
+                        required={true}
+                    />
+                    <Textarea
+                        ref={description}
+                        onChange={() => setServerError("")}
+                        placeholder={"Описание товара"}
+                        required={true}
+                    />
+                    <Textarea
+                        ref={characteristic}
+                        onChange={() => setServerError("")}
+                        placeholder={"характеристики товара"}
+                        required={true}
+                    />
+                    <div className="self-end">
+                        <ButtonBlue type={"submit"}>добавить</ButtonBlue>
+                    </div>
                 </form>
             </div>
         </>
