38f96df418388a4e56d7e2033391acb4ee016104
AlexEngo <a.o.engovatov@yandex.ru>
2024-05-04 21:07:29 +0400

    add dispatch category in search

--- frontend/src/pages/catalog/components/category-item/category-item.jsx
+++ frontend/src/pages/catalog/components/category-item/category-item.jsx
@@ -1,11 +1,9 @@
-import { Link } from "react-router-dom";
-
-export const CategoryItem = ({ url, children }) => {
+export const CategoryItem = ({ children, onSelectCategory }) => {
     return (
-        <Link to={url}>
-            <div className="px-[15px] py-[10px] rounded-lg transition-all duration-200 hover:bg-gray hover:text-black">
+        <div onClick={() => onSelectCategory(children)}>
+            <div className="px-[15px] py-[10px] rounded-lg cursor-pointer transition-all duration-200 hover:bg-gray hover:text-black">
                 {children}
             </div>
-        </Link>
+        </div>
     );
 };
--- frontend/src/pages/catalog/components/category-panel/category-panel.jsx
+++ frontend/src/pages/catalog/components/category-panel/category-panel.jsx
@@ -1,25 +1,30 @@
-import { Link } from "react-router-dom";
 import { CategoryItem } from "../category-item/category-item";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { getCategory } from "../../../../store/categorySlice";
+import { setCategoryName } from "../../../../store/searchProductSlice";
 
 export const CategoryPanel = () => {
-    const categories = useSelector(getCategory);
+    const dispatch = useDispatch();
+    const categoryList = useSelector(getCategory);
+
+    const onSelectCategory = (name) => {
+        dispatch(setCategoryName(name));
+    };
 
     return (
         <div className="flex flex-col w-full bg-lightGray p-[10px] mr-[10px] rounded-xl">
-            {categories.map(({ id, name }) => (
-                <CategoryItem key={id} url={`/catalog?category=${name}`}>
+            {categoryList.map(({ id, name }) => (
+                <CategoryItem key={id} onSelectCategory={onSelectCategory}>
                     {name}
                 </CategoryItem>
             ))}
 
-            <Link
-                to={"/catalog"}
-                className="text-center px-[15px] py-[10px] text-blue rounded-lg hover:underline"
+            <div
+                onClick={() => onSelectCategory("")}
+                className="text-center px-[15px] py-[10px] cursor-pointer text-blue rounded-lg hover:underline"
             >
                 все категории
-            </Link>
+            </div>
         </div>
     );
 };
