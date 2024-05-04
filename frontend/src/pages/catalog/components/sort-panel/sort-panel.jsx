8898b7ff328a6aae86f5dcc18fdf976162ea26a4
AlexEngo <a.o.engovatov@yandex.ru>
2024-05-04 20:09:57 +0400

    add dispatch searchPhrase

--- frontend/src/pages/catalog/components/sort-panel/sort-panel.jsx
+++ frontend/src/pages/catalog/components/sort-panel/sort-panel.jsx
@@ -1,18 +1,28 @@
 import { useDispatch, useSelector } from "react-redux";
-import { getSortPrice, setSortPrice } from "../../../../store/searchProductSlice";
+import {
+    getSearchPhrase,
+    getSortPrice,
+    setSearchPhrase,
+    setSortPrice,
+} from "../../../../store/searchProductSlice";
 
 export const SortPanel = () => {
     const dispatch = useDispatch();
-    const currentSortValue = useSelector(getSortPrice);
+    const currentSort = useSelector(getSortPrice);
+    const currentSearchPhrase = useSelector(getSearchPhrase);
 
     const onSortChange = ({ target }) => {
         dispatch(setSortPrice(target.value));
     };
 
+    const onSearchPhraseChange = ({ target }) => {
+        dispatch(setSearchPhrase(target.value));
+    };
+
     return (
         <div className="flex justify-end mb-[15px]">
             <select
-                value={currentSortValue}
+                value={currentSort}
                 onChange={(target) => onSortChange(target)}
                 className="outline-none text-darkGray border-2 px-[7px] py-[3px] border-gray rounded-lg mr-[10px] focus:border-blue"
             >
@@ -20,6 +30,8 @@ export const SortPanel = () => {
                 <option value="-1">сначала дороже</option>
             </select>
             <input
+                value={currentSearchPhrase}
+                onChange={(target) => onSearchPhraseChange(target)}
                 className="outline-none text-darkGray px-[7px] py-[3px] border-2 border-gray rounded-lg focus:border-blue"
                 type="text"
                 placeholder="поиск..."
