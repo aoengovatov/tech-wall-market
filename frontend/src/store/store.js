2492f0cc56f92f4349197b61124f6bfcfe954f36
AlexEngo <a.o.engovatov@yandex.ru>
2024-05-04 19:21:37 +0400

    update store

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import userReducers from "./userSlice";
 import categoryReducers from "./categorySlice";
 import modalReducers from "./modalSlice";
+import searchProductReducers from "./searchProductSlice";
 
 export const store = configureStore({
     reducer: {
         user: userReducers,
         category: categoryReducers,
         modal: modalReducers,
+        searchProduct: searchProductReducers,
     },
     devTools: true,
 });
