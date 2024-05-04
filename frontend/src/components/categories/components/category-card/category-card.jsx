256a53e3240c6e058ddb4f7099ab563fd1d4a2f3
AlexEngo <a.o.engovatov@yandex.ru>
2024-05-04 21:33:46 +0400

    refactoring categories

--- frontend/src/components/categories/components/category-card/category-card.jsx
+++ frontend/src/components/categories/components/category-card/category-card.jsx
@@ -1,12 +1,14 @@
 import { useDispatch } from "react-redux";
 import { ButtonDelete, ButtonEdit } from "../../../../components";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { setOpenModal } from "../../../../store/modalSlice";
 import { deleteCategory } from "../../../../store/categorySlice";
 import { request } from "../../../../utils";
+import { setCategoryName } from "../../../../store/searchProductSlice";
 
 export const CategoryCard = ({ id, src, color, edit, children }) => {
     const dispatch = useDispatch();
+    const navigate = useNavigate();
 
     const deleteCategoryById = (id) => {
         request(`/categories/${id}`, "DELETE").then((data) => {
@@ -26,10 +28,15 @@ export const CategoryCard = ({ id, src, color, edit, children }) => {
         dispatch(setOpenModal(modalWindow));
     };
 
+    const onClickCategory = () => {
+        dispatch(setCategoryName(children));
+        return navigate("/catalog");
+    };
+
     return (
-        <Link
-            to={edit ? null : `/catalog?caterory=${children}`}
-            className="flex w-[48%] h-[100px] items-center justify-end  mb-[10px] rounded-xl shadow-md relative z-0 transition-all duration-150 hover:scale-105 sm:h-[130px] md:h-[150px] md:w-[30%] lg:w-[255px]"
+        <div
+            onClick={edit ? null : onClickCategory}
+            className="flex w-[48%] h-[100px] items-center justify-end  mb-[10px] rounded-xl shadow-md relative z-0 cursor-pointer transition-all duration-150 hover:scale-105 sm:h-[130px] md:h-[150px] md:w-[30%] lg:w-[255px]"
             style={{ backgroundColor: color }}
         >
             {edit && (
@@ -42,8 +49,10 @@ export const CategoryCard = ({ id, src, color, edit, children }) => {
                     </div>
                 </>
             )}
-            <h2 className="font-medium text-[18px] absolute bottom-3 left-7 z-20 sm:text-[22px]">{children}</h2>
+            <h2 className="font-medium text-[18px] absolute bottom-3 left-7 z-20 sm:text-[22px]">
+                {children}
+            </h2>
             <img src={src} className="h-[50%] pr-[20px] z-10 sm:h-[60%] lg:h-[70%]"></img>
-        </Link>
+        </div>
     );
 };
