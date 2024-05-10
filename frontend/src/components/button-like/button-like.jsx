8317363e50bc10834f9a0e78b29e04e41acf1991
AlexEngo <a.o.engovatov@yandex.ru>
2024-05-10 15:48:12 +0400

    refactoring button-like component

--- frontend/src/components/button-like/button-like.jsx
+++ frontend/src/components/button-like/button-like.jsx
@@ -1,10 +1,12 @@
-export const ButtonLike = ({ ...props }) => {
+export const ButtonLike = ({ favoriteFlag = false, ...props }) => {
+    const style = favoriteFlag ? "grayscale-0" : "grayscale opacity-50";
+
     return (
         <button
             {...props}
-            className="border-0 w-[40px] p-[8px] rounded-full transition-all duration-200 hover:bg-lightGray"
+            className={`${style} border-0 w-[40px] p-[8px] rounded-full transition-all duration-200 hover:bg-lightGray hover:opacity-100`}
         >
-            <img src="/src/assets/like-icon-sm.png" className=""></img>
+            <img src="/src/assets/favorites.png" className=""></img>
         </button>
     );
 };
