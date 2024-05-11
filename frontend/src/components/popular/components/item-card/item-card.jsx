a89ee57b6d837d15204d1f65b78a599affe908ff
AlexEngo <a.o.engovatov@yandex.ru>
2024-05-11 08:53:03 +0400

    load popular products

--- frontend/src/components/popular/components/item-card/item-card.jsx
+++ frontend/src/components/popular/components/item-card/item-card.jsx
@@ -2,25 +2,29 @@ import { Link } from "react-router-dom";
 import { SaleWidget } from "../../../sale-widget/sale-widget";
 import { CardPrice } from "../../../card-price/card-price";
 import { ButtonBlue } from "../../../button-blue/button-blue";
+import { oldPriceCount } from "../../../../utils";
 
-export const ItemCard = () => {
+export const ItemCard = ({ id, name, price, sale, imageUrl }) => {
+    let oldPrice = 0;
+
+    if (sale > 0) {
+        oldPrice = oldPriceCount(price, sale);
+    }
+
     return (
         <div className="flex flex-col items-center justify-around max-w-[100%] min-w-[250px] min-h-[270px] border-2 shadow-md border-midGray rounded-xl h-[100px] p-[15px] transition-all duration-300 hover:border-lightBlue relative min-[540px]:max-w-[48%] md:min-h-[310px] min-[1095px]:max-w-[250px]">
             <div className="absolute top-2 right-2">
-                <SaleWidget count={30} />
+                <SaleWidget count={sale} />
             </div>
 
-            <img src="/src/assets/item-logo-min.png" className="w-[155px] md:w-[185px]"></img>
+            <img src={imageUrl} className="max-w-[155px] max-h-[120px] md:max-w-[185px]"></img>
             <Link to={"/catalog"}>
-                <div className="leading-5">
-                    15,3" Ноутбук Apple MacBook Air 2023 (M2) 8/512 Гб, Space
-                    Gray(Космический серый)
-                </div>
+                <div className="leading-5">{name}</div>
             </Link>
 
             <div className="flex items-center justify-between w-full">
                 <div className="flex flex-col">
-                    <CardPrice price={149990} oldPrice={189990} />
+                    <CardPrice price={price} oldPrice={oldPrice} />
                 </div>
                 <ButtonBlue>в корзину</ButtonBlue>
             </div>
--- frontend/src/components/popular/popular.jsx
+++ frontend/src/components/popular/popular.jsx
@@ -1,16 +1,35 @@
+import { useState, useEffect } from "react";
 import { ButtonRed } from "../button-red/button-blue";
 import { ItemCard } from "./components";
 import { Link } from "react-router-dom";
+import { request } from "../../utils";
 
 export const Popular = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        request("/products/top").then(({ error, products }) => {
+            if (error === null) {
+                setProducts(products);
+                console.log(products);
+            }
+        });
+    }, []);
+
     return (
         <div className="mb-[30px]">
             <h1 className="ml-[10px] mb-[15px]">Популярные товары</h1>
             <div className="flex w-[100%] justify-center mb-[15px] flex-wrap gap-3 ">
-                <ItemCard />
-                <ItemCard />
-                <ItemCard />
-                <ItemCard />
+                {products.map(({ _id: id, name, price, sale, imageUrl }) => (
+                    <ItemCard
+                        key={id}
+                        id={id}
+                        name={name}
+                        price={price}
+                        sale={sale}
+                        imageUrl={imageUrl}
+                    />
+                ))}
             </div>
             <div className="w-7/12 mx-auto sm:w-5/12 md:w-3/12">
                 <Link to="/catalog">
