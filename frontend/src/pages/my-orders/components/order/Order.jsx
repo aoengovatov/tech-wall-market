8f2f379d8836499916d4a872840c240fefc2f627
AlexEngo <a.o.engovatov@yandex.ru>
2024-05-14 19:19:01 +0400

    fix styles my-order page

--- frontend/src/pages/my-orders/components/order/Order.jsx
+++ frontend/src/pages/my-orders/components/order/Order.jsx
@@ -6,23 +6,24 @@ export const Order = ({ id, createdAt, status, products, totalPrice }) => {
         <div className="px-[20px] py-[15px] mb-[20px] bg-lightGray rounded-2xl">
             <div className="mb-[15px] flex align-top justify-between">
                 <div className="flex flex-col">
-                    <h2>Заказ № {id.slice(-8)}</h2>
+                    <h2 className="leading-7">Заказ № {id.slice(-8)}</h2>
                     <div className="text-darkGray">
                         {datetimeStringFormatter(createdAt)}
                     </div>
                 </div>
                 <div className="flex flex-col items-end">
-                    <div className="text-[22px]">{status}</div>
+                    <div className="text-[18px] text-right leading-6 md:text-[22px]">{status}</div>
                     <div className="text-darkGray">текущий статус</div>
                 </div>
             </div>
-            <div className="grid grid-cols-5 content-center justify-items-center gap-4 mb-[20px]">
-                {products.map(({ _id: id, name, price, count, imageUrl }) => (
+            <div className="grid grid-cols-1 content-center justify-items-center gap-3 mb-[20px] min-[390px]:grid-cols-2 min-[550px]:grid-cols-3 min-[550px]:gap-4 min-[740px]:grid-cols-4 min-[970px]:grid-cols-5">
+                {products.map(({ _id: id, name, price, productId, count, imageUrl }) => (
                     <ProductCard
                         key={id}
                         id={id}
                         name={name}
                         price={price}
+                        productId={productId}
                         count={count}
                         imageUrl={imageUrl}
                     />
--- frontend/src/pages/my-orders/components/product-card/product-card.jsx
+++ frontend/src/pages/my-orders/components/product-card/product-card.jsx
@@ -1,12 +1,18 @@
 import { Link } from "react-router-dom";
 
-export const ProductCard = ({ id, name, price, count, imageUrl }) => {
+export const ProductCard = ({ id, name, price, productId, count, imageUrl }) => {
     return (
-        <div className="flex flex-col p-[10px] bg-white rounded-xl w-[170px] hmin-[140px]">
-            <img src={imageUrl} className="h-[70px] mx-[20px] mb-[10px]"></img>
-            <Link to={`/catalog/${id}`} className="text-xs mb-[5px] font-normal">
-                {name}
-            </Link>
+        <div className="flex flex-col justify-between p-[10px] bg-white rounded-xl min-h-[200px]">
+            <div className="mb-[5px]">
+                <div className="w-full">
+                    <img src={imageUrl} className="h-[70px] mx-auto my-[14px]" />
+                </div>
+
+                <Link to={`/catalog/${productId}`} className="text-[14px] leading-3">
+                    {name}
+                </Link>
+            </div>
+
             <div className="text-[18px] self-end">
                 <div className="flex items-center">
                     <span>{count}</span>
