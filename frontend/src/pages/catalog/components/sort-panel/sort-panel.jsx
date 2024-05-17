cc9f7b08334a32887f1c32a3de2bac046f254675
AlexEngo <a.o.engovatov@yandex.ru>
2024-05-17 18:56:56 +0400

    add debounce in sort-panel component

--- frontend/src/pages/catalog/components/sort-panel/sort-panel.jsx
+++ frontend/src/pages/catalog/components/sort-panel/sort-panel.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect, useMemo } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import {
     getSearchPhrase,
@@ -5,24 +6,48 @@ import {
     setSearchPhrase,
     setPriceSort,
 } from "../../../../store/catalogSlice";
+import { DEBOUNCE_DELAY_LIMIT } from "../../../../constants";
+import { debounce } from "../../../../utils";
 
 export const SortPanel = () => {
     const dispatch = useDispatch();
-    const currentPriceSort = useSelector(getPriceSort);
-    const currentSearchPhrase = useSelector(getSearchPhrase);
+    const [shouldSort, setShouldSort] = useState(false);
+    const [updateSort, setUpdateSort] = useState(useSelector(getPriceSort));
+    const [shouldSearch, setShouldSearch] = useState(false);
+    const [updateSearch, setUpdateSearch] = useState(useSelector(getSearchPhrase));
+
+    useEffect(() => {
+        dispatch(setPriceSort(updateSort));
+    }, [shouldSort]);
+
+    useEffect(() => {
+        dispatch(setSearchPhrase(updateSearch));
+    }, [shouldSearch]);
+
+    const startDelayedSetSort = useMemo(
+        () => debounce(setShouldSort, DEBOUNCE_DELAY_LIMIT),
+        []
+    );
 
     const onSortChange = ({ target }) => {
-        dispatch(setPriceSort(target.value));
+        setUpdateSort(target.value);
+        startDelayedSetSort(!shouldSort);
     };
 
+    const startDelayedSetSearch = useMemo(
+        () => debounce(setShouldSearch, DEBOUNCE_DELAY_LIMIT),
+        []
+    );
+
     const onSearchPhraseChange = ({ target }) => {
-        dispatch(setSearchPhrase(target.value));
+        setUpdateSearch(target.value)
+        startDelayedSetSearch(!shouldSearch)
     };
 
     return (
         <div className="flex flex-col justify-end items-end mb-[10px] min-[450px]:flex-row">
             <select
-                value={currentPriceSort}
+                value={updateSort}
                 onChange={(target) => onSortChange(target)}
                 className="outline-none text-darkGray border-2 px-[7px] py-[3px] border-gray rounded-lg mr-0 mb-[5px] focus:border-blue min-[450px]:mr-[10px] min-[450px]:mb-0"
             >
@@ -30,7 +55,7 @@ export const SortPanel = () => {
                 <option value={false}>сначала дороже</option>
             </select>
             <input
-                value={currentSearchPhrase}
+                value={updateSearch}
                 onChange={(target) => onSearchPhraseChange(target)}
                 className="outline-none text-darkGray px-[7px] py-[3px] border-2 border-gray rounded-lg focus:border-blue"
                 type="text"
